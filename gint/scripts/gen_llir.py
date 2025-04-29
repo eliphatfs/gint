6d62a0715ab4d0fdf18ff2c08650c8a5135db4e1
eliphatfs <shiruoxi61@gmail.com>
2025-04-29 13:56:55 -0700

    Verify LLIR utility.

--- gint/scripts/gen_llir.py
+++ gint/scripts/gen_llir.py
@@ -22,10 +22,21 @@ def invoke_clang_shim(llir: bytes, target: Literal['ptx'] = 'ptx', cc: Optional[
     ], input=llir)
 
 
+def invoke_opt_shim(llir: bytes):
+    return subprocess.check_output([
+        'opt',
+        '-passes=verify',
+        '-S',
+        '-'
+    ], input=llir)
+    
+
+
 def main():
     argp = argparse.ArgumentParser('gint-gen-llir')
     argp.add_argument("-o", "--output-path", type=str, default=None)
     argp.add_argument("-t", "--target", type=str, default="llir", choices=['llir', 'ptx'])
+    argp.add_argument("-c", "--check-only", action='store_true', help='Check the original LLIR only and do not compile.')
     argp.add_argument(
         "-E", "--emit-llir",
         action='store_true',
@@ -36,7 +47,9 @@ def main():
     
     mod = build_interpreter_main_nvptx()
     ir = str(mod).encode()
-    if args.target != 'llir':
+    if args.check_only:
+        ir = invoke_opt_shim(ir)
+    elif args.target != 'llir':
         ir = invoke_clang_shim(ir, args.target, args.cc, args.emit_llir)
     if args.output_path is None:
         rich.print(ir.decode().strip().replace('[', '\\['))
