d4334dee34036aa6464b72d9fe7fe620a02491aa
eliphatfs <shiruoxi61@gmail.com>
2025-04-29 13:56:20 -0700

    BF16 type and global-restricted pointer.

--- gint/kernel/platforms/common.py
+++ gint/kernel/platforms/common.py
@@ -1,4 +1,22 @@
 from llvmlite import ir
+from llvmlite.ir.types import _BaseFloatType
+
+
+class BFloat16Type(_BaseFloatType):
+    """
+    The type for single-precision floats.
+    """
+    null = '0.0'
+    intrinsic_name = 'bf16'
+
+    def __str__(self):
+        return 'bfloat'
+
+    def format_constant(self, value):
+        raise ValueError("BF16 constants not supported. Use conversion from FP32 instead!")
+
+
+BFloat16Type._create_instance()
 
 
 i1 = ir.IntType(1)  # bool
@@ -8,6 +26,8 @@ i64 = ir.IntType(64)
 f16 = ir.HalfType()
 f32 = ir.FloatType()
 f64 = ir.DoubleType()
+bf16 = BFloat16Type()
 void = ir.VoidType()
 
 p_i8 = i8.as_pointer()
+p_i8g = i8.as_pointer(1)
